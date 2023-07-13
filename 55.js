32333548a04b9bf90773fd354c984ec28fd0c0f5
Neeraj Kumar <nkb.coder@gmail.com>
2023-07-13 17:12:46 +0530

    Add files via upload

---
+++ 55.js
@@ -0,0 +1,7 @@
+        function fruit() {
+            console.log(name);
+            console.log(price);
+            var name = "apple"
+            let price = 20;
+        }
+        // var undefined and error in let because rule of hosting let needs to be declared first and then used
